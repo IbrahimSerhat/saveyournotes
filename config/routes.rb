426c443811cd7e7267e41eb34cca30eea828378b
info <info@ibrahimserhat.com>
2018-09-22 05:47:32 -0400

    fix signed user root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'welcome#index'
+  devise_for :users  
 	
 	authenticated :user do
-		root 'docs#index', as: 'authenticated_root'
+		root 'docs#index', as: :authenticated_root
 	end
+	
+	root 'welcome#index'
+	
 	resources :docs
 end
